b637064447a5060621589069295fa542de111f01
Miguel Angel Ortiz Neira <miguel1516q6@gmail.com>
2024-08-04 23:49:40 -0500

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,9 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router'
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+const RootLayout = () => {
+  return <Slot />
 }
+
+export default RootLayout
