01c6f6ced3cad0eaf3bcea699887662c289c9b88
Miguel Angel Ortiz Neira <miguel1516q6@gmail.com>
2024-08-05 20:22:53 -0500

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
-import { Slot } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { Link } from 'expo-router'
 
-const RootLayout = () => {
-  return <Slot />
+
+export default function App(){
+  return(
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Aora ADSO</Text>
+      <StatusBar style='auto' />
+      <Link href={'/profile'} className='text-blue-500'>Go to profile</Link>
+    </View>
+  )
 }
-
-export default RootLayout
